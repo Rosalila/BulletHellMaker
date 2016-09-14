d32eb1fa61c4c72fdb7533d7e8e570a697da36d3
Turupawn <ahmed.hn.43@gmail.com>
2016-09-14 13:16:18 -0600

    I wish this fixes the steam controller issue

--- RosalilaShooter/Menus/ControllerConfig.cpp
+++ RosalilaShooter/Menus/ControllerConfig.cpp
@@ -83,7 +83,7 @@ map<string,Button*> ControllerConfig(bool reconfigure)
                 key_pressed=i;
             }
         }
-        for(int i=0;i<20;i++)
+        for(int i=0;i<100;i++)
         {
             if(receiver->isJoyPressed(i,0))
             {
