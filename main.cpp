4b3bb7ca7d87b3f801b6af3573cf614ab87679b3
Turupawn <ahmed.hn.43@gmail.com>
2016-09-16 22:49:13 -0600

    steam controller interface ready, it's time to get nasty

--- main.cpp
+++ main.cpp
@@ -22,7 +22,6 @@ int main(int argc, char *argv[])
     }
 
     RosalilaInit();
-    Rosalila()->Utility->clearLog();
     initShooterUtility();
 
     Rosalila()->ApiIntegrator->unlockAchievement("A");
